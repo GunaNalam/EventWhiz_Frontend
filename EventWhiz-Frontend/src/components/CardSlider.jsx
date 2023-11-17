dfeea9dde3bd562909c6057ae150ad05418003ac
Nalam Guna Sri Krishna <102983513+GunaNalam@users.noreply.github.com>
2023-11-17 13:39:11 +0530

    CardSlider.jsx

--- EventWhiz-Frontend/src/components/CardSlider.jsx
+++ EventWhiz-Frontend/src/components/CardSlider.jsx
@@ -6,7 +6,39 @@ import './CardSlider.css';
 import { FaCalendarDays, FaLocationDot } from "react-icons/fa6";
 
 const CardSlider = () => {
-  const cardData = ["Card 1", "Card 2", "Card 3", "Card 4", "Card 5"];
+  const cardData = [
+    {
+      "Title": "IIFA Awards",
+      "Description": "Bollywood's prestigious film awards, globally celebrating excellence in Indian cinema.",
+      "City": "Mumbai",
+      "Date": "22-09-2019"
+    },
+    {
+      "Title": "World Cup",
+      "Description": "The pinnacle of international cricket, featuring the world's best teams in a thrilling final match.",
+      "City": "London",
+      "Date": "14-07-2019"
+    },
+    {
+      "Title": "Navratri Festival",
+      "Description": "A vibrant nine-night Hindu festival celebrating the goddess Durga through dance and music.",
+      "City": "Various",
+      "Date": "29-09-2019"
+    },
+    {
+      "Title": "Ganesh Chaturthi",
+      "Description": "Hindu festival honoring Lord Ganesha, marked by elaborate idol processions and cultural events.",
+      "City": "Mumbai",
+      "Date": "02-09-2019"
+    },
+    {
+      "Title": "Auto Expo",
+      "Description": "India's largest automobile exhibition showcasing the latest innovations and trends in the automotive industry.",
+      "City": "Greater Noida",
+      "Date": "07-02-2020"
+    }
+  ];
+  
   const [currentSlide, setCurrentSlide] = useState(0);
   const sliderRef = useRef(null);
 
@@ -33,16 +65,18 @@ const CardSlider = () => {
             <div className="carousel-body">
               <div className="carousel-body-sub">
                 <div className="CarouselBodyHeading">
-                  Event Title 
+                  {card.Title}
                 </div>  
                 <div className="CarouselBodyDesc">
-                  Event Description - Ex: Lorem ipsum ng elit. Aliquam, laudantium.
-                </div>  
+                  {card.Description} 
+                </div> 
                 <div className="CarouselBodyLocDate">
-                  <FaCalendarDays /> 20-11-2023
+                  <FaCalendarDays />
+                  {card.Date} 
                 </div>
                 <div className="CarouselBodyLocDate">
-                  <FaLocationDot /> Vizag
+                  <FaLocationDot />
+                  {card.City} 
                 </div>
                 <button className="btn btn-primary my-3 rounded-pill">Attend</button>
               </div>
